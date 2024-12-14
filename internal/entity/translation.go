a2be8c3e80953e191fb79ce176dc990b05cf60bc
LiebDenisa <denisalieb@gmail.com>
2024-12-14 15:59:29 +0200

    chore: init

--- internal/entity/translation.go
+++ internal/entity/translation.go
@@ -9,3 +9,11 @@ type Translation struct {
 	Original    string `json:"original"     example:"текст для перевода"`
 	Translation string `json:"translation"  example:"text for translation"`
 }
+type Product struct {
+	ID          string `json:"id" example:"1"`
+	Name        string `json:"name" example:"Darius"`
+	Description string `json:"description" example:"A great product"`
+	Price       int    `json:"price" example:"100"`
+	CreatedAt   string `json:"created_at" example:"2020-01-01"`
+	UpdatedAt   string `json:"updated_at" example:"2020-01-01"`
+}
